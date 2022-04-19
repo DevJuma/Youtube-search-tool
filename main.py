bccbfec60edd57f3ba002ff5ea54131c2a5b1727
DevJuma <Social.julian.c.m@gmail.com>
2022-04-19 03:07:44 +0200

    03:07 04.19.2022

--- main.py
+++ main.py
@@ -10,17 +10,21 @@ root.geometry("240x120+500+200")
 
 def button_command1():
     text = entry1.get()
-    i = int(entry2.get())
-    if i < 100:
+    i = int(entry2.get()) - 1
+    if (i < 0) or (i > 19):
+        lb4 = Label(root, text="Number between 1 and 20")  # error message
+        lb4.place(x=80, y=60)
+    else:
         html = urllib.request.urlopen("https://www.youtube.com/results?search_query=" + text)
         vidid = re.findall(r"watch\?v=(\S{11})", html.read().decode())
-        entry3.delete(0, END)
-        entry3.insert(0, ("https://www.youtube.com/watch?v=" + vidid[i]))
+        linktovid = "https://www.youtube.com/watch?v=" + vidid[i]
+        root.clipboard_clear()
+        root.clipboard_append(linktovid)
+        lb4 = Label(root, text=linktovid)
+        lb4.place(x=80, y=60)
         entry1.delete(0, END)
         entry2.delete(0, END)
         return None
-    else:
-        entry3.insert(0, "Not more than 2 digits")  # error message
 
 
 btn1 = Button(root, text="Search", fg='Purple', bg='Yellow', command=button_command1)
@@ -29,7 +33,6 @@ lb2 = Label(root, text='Video no.:')
 lb3 = Label(root, text='result:')
 entry1 = Entry(root, width=20)
 entry2 = Entry(root, width=2)
-entry3 = Entry(root)
 
 btn1.place(x=10, y=85)
 lb1.place(x=10, y=10)
@@ -37,6 +40,6 @@ lb2.place(x=10, y=35)
 lb3.place(x=10, y=60)
 entry1.place(x=80, y=10)
 entry2.place(x=80, y=35)
-entry3.place(x=80, y=60)
+
 
 root.mainloop()
